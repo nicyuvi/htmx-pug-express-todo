20a27461cf75ff58577cd8abac6407b470e4dc01
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-01-31 11:37:42 -0800

    update prisma schema. add about page. add nav bar

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const { PrismaClient } = require('@prisma/client')
+const db = new PrismaClient()
 
 const payload = {
   title: 'heading 1',
   message: 'using pug',
   subtitle: 'subtitle',
-};
+}
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', payload);
-});
+router.get('/', async (req, res, next) => {
+  // primsa queries
+  res.render('index', payload)
+})
 
-module.exports = router;
+/* GET about page. */
+router.get('/about', async (req, res, next) => {
+  // primsa queries
+  res.render('about')
+})
+
+module.exports = router
