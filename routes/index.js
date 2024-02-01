33bef1f1e5671e641c88fc29bec8c35df078730a
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-02-01 08:57:20 -0800

    WIP: active states commented out. htmx replaces dom

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,28 @@ var router = express.Router()
 const { PrismaClient } = require('@prisma/client')
 const db = new PrismaClient()
 
-const payload = {
-  title: 'heading 1',
-  message: 'using pug',
-  subtitle: 'subtitle',
-}
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const todos = await db.todo.findMany()
+  // const active = { home: 'home' }
   res.render('index', { todos })
 })
 
 /* GET about page. */
 router.get('/about', async (req, res, next) => {
+  // const active = { about: 'about' }
   res.render('about')
+})
+
+router.post('/create', async (req, res, next) => {
+  const todo = await db.todo.create({
+    data: {
+      content: req.body.todo,
+    },
+  })
+  console.log('created todo', todo)
+  const { content } = todo
+  res.render('test', { content })
 })
 
 module.exports = router
